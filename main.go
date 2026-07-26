cc94fdb10b9e5d601a24d8db2347d17f5fa49169
Naveen <ippilinaveen741@gmail.com>
2026-07-26 15:53:49 +0530

    Added application logs

--- main.go
+++ main.go
@@ -6,26 +6,29 @@ import (
 )
 
 func homePage(w http.ResponseWriter, r *http.Request) {
-	// Render the home html page from static folder
+	log.Println("Home page accessed")
 	http.ServeFile(w, r, "static/home.html")
 }
 
 func coursePage(w http.ResponseWriter, r *http.Request) {
-	// Render the course html page
+	log.Println("Courses page accessed")
 	http.ServeFile(w, r, "static/courses.html")
 }
 
 func aboutPage(w http.ResponseWriter, r *http.Request) {
-	// Render the about html page
+	log.Println("About page accessed")
 	http.ServeFile(w, r, "static/about.html")
 }
 
 func contactPage(w http.ResponseWriter, r *http.Request) {
-	// Render the contact html page
+	log.Println("Contact page accessed")
 	http.ServeFile(w, r, "static/contact.html")
 }
 
 func main() {
+
+	log.Println("Go Web App Started")
+	log.Println("Listening on port 8080")
 
 	http.HandleFunc("/home", homePage)
 	http.HandleFunc("/courses", coursePage)
